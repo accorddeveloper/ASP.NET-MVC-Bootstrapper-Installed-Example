bacc5e48292ebb9bdacf5795a196848116ce5f92
Alim Ul Karim <devorg.bd@gmail.com>
2015-10-04 22:28:10 +0600

    Complete system with school management
    Complete system with school management

--- ReliSource/Controllers/PartialsController.cs
+++ ReliSource/Controllers/PartialsController.cs
@@ -1,13 +1,11 @@
-﻿//using ReliSource.Models.EntityModel.POCO; // Northwind Sample
-using DevTrends.MvcDonutCaching;
-using ReliSource.Models.EntityModel;
+﻿using ReliSource.Models.EntityModel;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace ReliSource.Controllers {
     [OutputCache(CacheProfile = "YearNoParam")]
     //public class PartialsController : GenericController<Inherit it with your db context> {
-    public class PartialsController : GenericController<NorthwindEntities> {
+    public class PartialsController : GenericController<SchoolEntities> {
 
 
         #region Constructors
@@ -19,63 +17,21 @@ namespace ReliSource.Controllers {
 
         #endregion
 
-        #region Nortwind test
-        //#region EmployeesController : DropDowns to paste into the partial
-
-        //// [DonutOutputCache(CacheProfile = "YearNoParam")]
-        //public JsonResult GetReportsTo() {
-        //    var data = db.Employees.Select(n => new { id = n.EmployeeID, display = n.LastName }).ToList();
-        //    return Json(data, JsonRequestBehavior.AllowGet);
-        //}
-
-        //#endregion
-
-        //#region ProductOrdersController : DropDowns to paste into the partial
-        //public JsonResult GetEmployeeID() {
-        //    var data = db.Employees.Select(n => new { id = n.EmployeeID, display = n.LastName + " (" + n.EmployeeID + ")" }).ToList();
-        //    return Json(data, JsonRequestBehavior.AllowGet);
-        //}
-        //// [DonutOutputCache(CacheProfile = "YearNoParam")]
-        //public JsonResult GetCustomerID() {
-        //    var data = db.Customers.Select(n => new { id = n.CustomerID, display = n.CompanyName }).ToList();
-        //    return Json(data, JsonRequestBehavior.AllowGet);
-        //}
-
-        //[OutputCache(CacheProfile = "Year")]
-        //public JsonResult GetShipVia(int id) {
-        //    var data = db.Shippers.Where(n=> n.ShipperID == id).Select(n => new { id = n.ShipperID, display = n.CompanyName }).ToList();
-        //    return Json(data, JsonRequestBehavior.AllowGet);
-        //}
-
-        //#endregion 
-        #endregion
-
-        #region EmployeesController : DropDowns to paste into the partial
+        #region SchoolClassesController : DropDowns to paste into the partial
 
         // [DonutOutputCache(CacheProfile = "YearNoParam")]
-        public JsonResult GetReportsTo() {
-            var data = db.Employees.Select(n => new { id = n.EmployeeID, display = n.LastName }).ToList();
+        public JsonResult GetSchoolID() {
+            var data = db.Schools.Select(n => new { id = n.SchoolID, display = n.SchoolName }).ToList();
             return Json(data, JsonRequestBehavior.AllowGet);
         }
 
         #endregion
-        #region ProductOrdersController : DropDowns to paste into the partial
+
+        #region SubjectsController : DropDowns to paste into the partial
 
         // [DonutOutputCache(CacheProfile = "YearNoParam")]
-        public JsonResult GetCustomerID() {
-            var data = db.Customers.Select(n => new { id = n.CustomerID, display = n.CompanyName }).ToList();
-            return Json(data, JsonRequestBehavior.AllowGet);
-        }
-
-        // [DonutOutputCache(CacheProfile = "YearNoParam")]
-        public JsonResult GetEmployeeID() {
-            var data = db.Employees.Select(n => new { id = n.EmployeeID, display = n.LastName }).ToList();
-            return Json(data, JsonRequestBehavior.AllowGet);
-        }
-
-        // [DonutOutputCache(CacheProfile = "YearNoParam")]
-        public JsonResult GetShipVia(int id) {
-            var data = db.Shippers.Where(n=> n.ShipperID == id).Select(n => new { id = n.ShipperID, display = n.CompanyName }).ToList();
+        public JsonResult GetSchoolClassID() {
+            var data = db.SchoolClasses.Select(n => new { id = n.SchoolClassID, display = n.ClassName }).ToList();
             return Json(data, JsonRequestBehavior.AllowGet);
         }
 
